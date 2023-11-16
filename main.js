0207538d3be27f514cea70be0208a9a5df5af893
Jenaya F <145370280+Shava-mp4@users.noreply.github.com>
2023-11-16 10:13:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+//Complete the Phrase
+
+document.getElementById("P1").addEventListener("click", phrase1);
+document.getElementById("P2").addEventListener("click", phrase2);
+document.getElementById("P3").addEventListener("click", phrase3);
+document.getElementById("P4").addEventListener("click", phrase4);
+document.getElementById("P5").addEventListener("click", phrase5);
+
+function phrase1() {
+  alert("bush.");
+}
+
+function phrase2() {
+  alert("corners.");
+}
+
+function phrase3() {
+  alert("doubt.");
+}
+
+function phrase4() {
+  alert("rocket");
+}
+
+function phrase5() {
+  alert("devil.");
+}
